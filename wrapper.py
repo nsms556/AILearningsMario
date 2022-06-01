d1309ec12cb389e110cff3c026734278e92d9d0c
nsms556 <nsms556@kakao.com>
2022-06-01 18:03:34 +0900

    add CustomReward Wrapper

--- wrapper.py
+++ wrapper.py
@@ -12,6 +12,7 @@ class SkipFrame(gym.Wrapper) :
     def __init__(self, env: Env, skip: int) -> None:
         super().__init__(env)
         self._skip = skip
+        self._obs_buffer = np.zeros((2,) + env.observation_space.shape, dtype=env.observation_space.dtype)
 
     def step(self, action) :
         total_reward = 0.0
@@ -19,12 +20,20 @@ class SkipFrame(gym.Wrapper) :
         
         for i in range(self._skip) :
             obs, reward, done, info = self.env.step(action)
+            
+            if i == self._skip - 2 :
+                self._obs_buffer[0] = obs
+            if i == self._skip - 1 :
+                self._obs_buffer[1] = obs
+            
             total_reward += reward
             
             if done :
                 break
 
-        return obs, total_reward, done, info
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, done, info
 
 class GrayScaleObservation(gym.ObservationWrapper) :
     transforms = T.Grayscale()
@@ -66,3 +75,21 @@ class ResizeObservation(gym.ObservationWrapper) :
         observation = transforms(observation).squeeze(0)
 
         return observation
+
+class CustomReward(gym.Wrapper) :
+    def __init__(self, env: Env):
+        super().__init__(env)
+        self._score = 0
+
+    def step(self, action) :
+        state, reward, done, info = self.env.step(action)
+        reward += (info['score'] - self._score) / 40.0
+        self._score = info['score']
+
+        if done :
+            if info['flag_get'] :
+                reward += 350.0
+            else :
+                reward -= 50.0
+
+        return state, reward / 10.0, done, info
